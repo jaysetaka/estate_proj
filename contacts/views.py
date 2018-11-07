8e82ea6e9e4e5d1047333ed03c028e5f6ecb1e3e
Jabulani Setaka <jsetaka@meditech.co.za>
2018-11-07 02:16:16 +0200

    final commit

--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-
+from django.core.mail import send_mail
 from .models import Contact
 
 
@@ -25,6 +25,18 @@ def contact(request):
                 return redirect('/listings/'+listing_id)
 
         contact = Contact(listing_id=listing_id, listing=listing, name=name,email=email, phone=phone, message=message, user_id=user_id)
+
         contact.save()
+
+        #Send mail
+
+        send_mail(
+            'Property Listing Inquiry',
+            'There has been an inquiry ' + listing + '. Sign into admin panel for more info',
+            'jabusetanezz@gmail.com',
+            [realtor_email, 'jsetaka@meditech.co.za'],
+            fail_silently=False,
+        )
+
         messages.success(request, 'Your request has been submitted, a realtor will get back to you soon')
         return redirect('/listings/'+listing_id)
