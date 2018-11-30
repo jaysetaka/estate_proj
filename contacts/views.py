10c69e8ea32275de906e0b3962b6910b6836304e
Jabulani Setaka <jsetaka@meditech.co.za>
2018-12-01 01:42:43 +0200

    email removed

--- contacts/views.py
+++ contacts/views.py
@@ -29,14 +29,13 @@ def contact(request):
         contact.save()
 
         #Send mail
-
-        send_mail(
-            'Property Listing Inquiry',
-            'There has been an inquiry ' + listing + '. Sign into admin panel for more info',
-            'jabusetanezz@gmail.com',
-            [realtor_email, 'jsetaka@meditech.co.za'],
-            fail_silently=False,
-        )
+        # send_mail(
+        #     'Property Listing Inquiry',
+        #     'There has been an inquiry ' + listing + '. Sign into admin panel for more info',
+        #     'jabusetanezz@gmail.com',
+        #     [realtor_email, 'jsetaka@meditech.co.za'],
+        #     fail_silently=False,
+        # )
 
         messages.success(request, 'Your request has been submitted, a realtor will get back to you soon')
         return redirect('/listings/'+listing_id)
